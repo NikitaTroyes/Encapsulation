5ca03bdd60998c2cf9a38669a102cf0ec4679700
NikitaTroyes <89306099+NikitaTroyes@users.noreply.github.com>
2021-09-30 08:29:54 +0200

    Modif fichier

--- BeerEncapsulator.cs
+++ BeerEncapsulator.cs
@@ -16,12 +16,18 @@ namespace Encapsulation
 
         public void SetAvalaibleBottles(int avalaibleBottles)
         {
-            _avalaibleBottles = avalaibleBottles;
+             if (avalaibleBottles >= 0)
+            {
+                _avalaibleBottles = avalaibleBottles;
+            }
         }
 
         public void SetAvalaibleCapsules(int avalaibleCapsules)
         {
-            _avalaibleCapsules = avalaibleCapsules;
+            if (avalaibleCapsules >= 0)
+            {
+                _avalaibleCapsules = avalaibleCapsules;
+            }
         }
 
         public double AddBeer (double addedBeerVolume)
@@ -34,6 +40,9 @@ namespace Encapsulation
         public int ProduceEncapsulatedBeerBottles(int nombreDeBouteillesARealiser)
         {              
             int completedBottles = 0;
+            int remainedBottles = nombreDeBouteillesARealiser;
+            int remainedCapsules = nombreDeBouteillesARealiser;
+            int remainedBeerVolumme = nombreDeBouteillesARealiser * 33;
 
             while (_avalaibleBottles > 0 && _avalaibleCapsules > 0 && _avalaibleBeerVolume > 33)
             {
@@ -41,6 +50,9 @@ namespace Encapsulation
                 _avalaibleCapsules = _avalaibleCapsules - 1;
                 _avalaibleBeerVolume = _avalaibleBeerVolume - 33;
                 completedBottles = completedBottles + 1;
+                remainedBottles = remainedBottles - 1;
+                remainedCapsules = remainedCapsules - 1;
+                remainedBeerVolumme = remainedBeerVolumme - 33; 
 
                 if (nombreDeBouteillesARealiser == completedBottles)
                 {
@@ -54,6 +66,24 @@ namespace Encapsulation
             {
                 Console.WriteLine(" Le nombre de bouteilles complètes souhaitées est supérieur à ce que peut fournir la machine actuellement. Veuillez recharger la machine");
             }
+            
+            
+            if (_avalaibleBottles <= 0)
+            {
+                  Console.WriteLine("Pour réaliser " + nombreDeBouteillesARealiser + "bouteilles, il faut : " + (remainedBottles) + " bouteilles en plus.");
+            }
+
+            if (_avalaibleCapsules <= 0)
+            {
+                 Console.WriteLine("Pour réaliser " + nombreDeBouteillesARealiser + "bouteilles, il faut : " + (remainedCapsules) + " capsules en plus. ");
+            }
+
+            if (_avalaibleBeerVolume <= 33)
+            {
+                 Console.WriteLine("Pour réaliser " + nombreDeBouteillesARealiser + "bouteilles, il faut : " + (remainedBeerVolumme) + " cL en plus");
+            }
+            
+                   
             Console.WriteLine("Le nombre de bouteilles complétées est : " + completedBottles);
 
             return completedBottles;
